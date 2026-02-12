6d52d3c36bdc187655c648ac4593351cda089511
PedroAlex65 <pedroalex663@gmail.com>
2026-02-12 13:07:26 -0300

    test: add unit tests and fix null handling for RepositoryMapperImpl #43. Adding a guard clause in RepositoryMapperImpl to handle null inputs, preventing NullPointerExceptions and ensuring a null result as requested

---
+++ src/test/java/com/github/regyl/gfi/mapper/RepositoryMapperImplTest.java
@@ -0,0 +1,55 @@
+package com.github.regyl.gfi.mapper;
+
+import com.github.regyl.gfi.controller.dto.github.issue.GithubRepositoryDto;
+import com.github.regyl.gfi.entity.RepositoryEntity;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.OffsetDateTime;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class RepositoryMapperImplTest {
+    @InjectMocks
+    private RepositoryMapperImpl repositoryMapper;
+
+    @Mock
+    private Supplier<OffsetDateTime> dateTimeSupplier;
+
+
+    @Test
+    public void resultNullTest() {
+        RepositoryEntity result = repositoryMapper.apply(null);
+        assertNull(result, "O Mapeamento de entrada nula deve retornar nulo");
+    }
+
+    @Test
+    public void shouldMapFullDtoToRepositoryEntity() {
+
+        GithubRepositoryDto dto = new GithubRepositoryDto();
+        dto.setId("1");
+        dto.setNameWithOwner("Regyl/project");
+        dto.setUrl("https://github.com/Regyl/gfi");
+        dto.setStargazerCount(100);
+        dto.setDescription("Um projeto icrivel");
+
+        OffsetDateTime now = OffsetDateTime.now();
+        Mockito.when(dateTimeSupplier.get()).thenReturn(now);
+
+        RepositoryEntity resultado = repositoryMapper.apply(dto);
+
+        assertNotNull(resultado, "The result must not be null.");
+        assertEquals("1", resultado.getSourceId());
+        assertEquals("Regyl/project", resultado.getTitle());
+        assertEquals("https://github.com/Regyl/gfi", resultado.getUrl());
+        assertEquals(100, resultado.getStars());
+        assertEquals("Um projeto icrivel", resultado.getDescription());
+        assertEquals(now, resultado.getCreated());
+    }
+}
