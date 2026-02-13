be3654af077c9ccd957e516d5c88377b666078e7
PedroAlex65 <pedroalex663@gmail.com>
2026-02-13 10:04:04 -0300

    refactor: use @InjectMocks for service instantiation (#40)

--- src/test/java/com/github/regyl/gfi/service/impl/feed/cyclonedx/homepage/github/GithubPurlToHomepageServiceImplTest.java
+++ src/test/java/com/github/regyl/gfi/service/impl/feed/cyclonedx/homepage/github/GithubPurlToHomepageServiceImplTest.java
@@ -3,13 +3,15 @@ package com.github.regyl.gfi.service.impl.feed.cyclonedx.homepage.github;
 import com.github.packageurl.PackageURL;
 import com.github.regyl.gfi.annotation.DefaultUnitTest;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DefaultUnitTest
 class GithubPurlToHomepageServiceImplTest {
 
-    private GithubPurlToHomepageServiceImpl service = new GithubPurlToHomepageServiceImpl();
+    @InjectMocks
+    private GithubPurlToHomepageServiceImpl service;
 
     @Test
     void shouldConvertPurlToGithubHomepage() throws Exception {
